153e2f5ea3d9c76bd5d2901a6604255e40ebba12
niranjkrishna <niranjk90@gmail.com>
2026-08-29 17:23:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,9 +11,5 @@ if st.button("Generate Report"):
     st.write("Profit: -₹10550")
 
 
-from pyngrok import ngrok
 
-ngrok.set_auth_token("3IaW6ryv9v0nqRUP2JjDqcEtZ2o_5a7L11vYDStaCqfRq4Vrh")
-public_url = ngrok.connect(8501)
 
-print(public_url)
