34a9e117e0fd16a3acb946920821236fb80d80eb
niranjkrishna <niranjk90@gmail.com>
2026-08-29 18:40:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ genai.configure(
     api_key=st.secrets["GEMINI_API_KEY"]
 )
 
-model = genai.GenerativeModel("gemini-2.5-flash")
+model = genai.GenerativeModel("gemini-3.6-flash")
 def ai_cfo(question):
     try:
         prompt = f"""
